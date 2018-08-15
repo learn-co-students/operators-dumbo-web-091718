86170f476bdfc81d54fd5b4b04d192af6258b6b2
Christopher Weber <chrisrrweber@gmail.com>
2018-08-15 23:47:01 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
 def unsafe?(speed)
-
+  if (speed < 40 || speed > 60)
+    return true
+  else speed == (40..60)
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
-end
-	
+	(speed < 40 || speed > 60) ? true : false end
 
 
+# puts (if 1 then 2 else 3 end) # => 2
+
+# puts 1 ? 2 : 3                # => 2
+
+# x = if 1 then 2 else 3 end
+# puts x                        # => 2
